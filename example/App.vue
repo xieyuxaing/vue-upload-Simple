d95c6091f91a1511e0d7389dec836877297a2f94
admin <285837460@qq.com>
2018-02-06 17:26:43 +0800

    自己修改封装提供的简单上传

---
+++ example/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <uploader :options="options" :file-status-text="statusText" class="uploader-example" ref="uploader"
+            @file-complete="fileComplete" @complete="complete"
+            @files-added="filesAdd" @file-added="fileAdded"
+  ></uploader>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        options: {
+          target: '//localhost:3000/upload', // '//jsonplaceholder.typicode.com/posts/',
+          testChunks: false
+        },
+        attrs: {
+          accept: 'image/*'
+        },
+        statusText: {
+          success: '成功了',
+          error: '出错了',
+          uploading: '上传中',
+          paused: '暂停中',
+          waiting: '等待中'
+        }
+      }
+    },
+    methods: {
+      complete () {
+        console.log('complete', arguments)
+      },
+      fileComplete () {
+        console.log('file complete', arguments)
+      },
+      filesAdd (fileList, newFileList, evt) {
+        if (newFileList.length > 0) {
+          if (newFileList[0].isFolder) {
+            for (var n in fileList) {
+              if (fileList[n].size > 4 * 1024) {
+                fileList[n].ignored = true
+              }
+            }
+          }
+        }
+      },
+      fileAdded (files) {
+        if (files.relativePath.split('/').length === 1) {
+          if (files.size > 4 * 1024) {
+            files.ignored = true
+          }
+        }
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        window.uploader = this.$refs.uploader.uploader
+      })
+    }
+  }
+</script>
+
+<style>
+  .uploader-example {
+    width: 880px;
+    padding: 15px;
+    margin: 40px auto 0;
+    font-size: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .4);
+  }
+
+  .uploader-example .uploader-btn {
+    margin-right: 4px;
+  }
+
+  .uploader-example .uploader-list {
+    max-height: 440px;
+    overflow: auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+</style>
